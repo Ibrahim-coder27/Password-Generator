d9241dc2bd973c7ffbfda795e4540106a6e9f721
Ibrahim Madakiya <ibrahimmadakiya9@gmail.com>
2026-04-16 20:04:04 +0530

    logic added

--- component/script.js
+++ component/script.js
@@ -1,4 +1,4 @@
-import {React,use,useState} from "react";
+import {React,useState,useEffect} from "react";
 import ReactDOM from "react-dom/client"
 
 function PasswordGenerator(){
@@ -7,17 +7,37 @@ function PasswordGenerator(){
     const [length,setLength] = useState(10);
     const [numberChecked,setNumberChanged] = useState(false);
     const [characterChecked,setCharacterChanged] = useState(false);
+
+    function passGenerator(){
+        let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if(numberChecked){
+            str+="0123456789";
+        }
+        if(characterChecked){
+            str+="!@#$%^&*()~`";
+        }
+        let pass="";
+        for (let index = 0; index < length; index++) {
+            
+            pass+= str[Math.floor(Math.random()*str.length)];
+        }
+        setPassword(pass);
+    }
+    useEffect(()=>{
+        passGenerator();
+    },[numberChecked,characterChecked,length])
+
     return (
         <>
             <h1>Password is {password}</h1>
             <div>
-                <input type="range" min={5} max={50} value={length} onChange={(e)=>{setLength(e.target.value)}}></input>
+                <input type="range" min={5} max={20} value={length} onChange={(e)=>{setLength(e.target.value)}}></input>
                 <label>Length is {length}</label>
 
-                <input type="checkbox" defaultChecked = {numberChecked}></input>
+                <input type="checkbox" defaultChecked = {numberChecked} onClick={(e)=>{setNumberChanged(!numberChecked)}}></input>
                 <label>Number</label>
 
-                <input type="checkbox" defaultChecked = {characterChecked}></input>
+                <input type="checkbox" defaultChecked = {characterChecked} onClick={(e)=>{setCharacterChanged(!characterChecked)}}></input>
                 <label>Character</label>
             </div>
         </>
